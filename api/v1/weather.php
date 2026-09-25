ba8b01c071a5efbbf3c189e1baa7e7356ae62426
yulcaribe <mehmetisaacar47@gmail.com>
2026-09-25 22:08:22 +0300

    Tag weather API v1 responses

--- api/v1/weather.php
+++ api/v1/weather.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+header('X-YC-API-Version: 1');
+header('X-YC-API-Resource: weather');
+
 // Canonical weather API. METAR/TAF currently share the existing resilient AWC
 // fetch/cache implementation; product-specific v1 endpoints can be added later
 // without changing clients that use this combined resource.
